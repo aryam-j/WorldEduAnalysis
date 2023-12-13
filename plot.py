9eca00a020300d7107864a910adbb973efea6139
aryam <aryamjay@gmail.com>
2023-12-14 03:50:31 +0530

    Added eda.py

--- plot.py
+++ plot.py
@@ -10,113 +10,140 @@ def get_countries_with_income(income):
     country_df = country_df[country_df['Currency Unit'].notna()]
     return set(country_df[country_df["Income Group"].isin(income)]["Table Name"].to_list())
 
-country_df = pd.read_csv('EdStatsCountry.csv')
-
+# country_df = pd.read_csv('EdStatsCountry.csv')
+country_list = ["United States",
+    "China",
+    "India",
+    "Brazil",
+    "Russia",
+    "Nigeria",
+    "Japan",
+    "Germany",
+    "Indonesia",
+    "Mexico",
+    "Egypt",
+    "South Africa",
+    "Australia",
+    "Canada",
+    "Sweden"]
 
 if __name__=="__main__":
     df = pd.read_csv("pivot.csv")
 
-
-    new_df = df[["SE.ADT.LITR.ZS", "NY.GDP.PCAP.PP.KD", "Country Name"]]
-
-    sns.scatterplot(x="SE.ADT.LITR.ZS", y="NY.GDP.PCAP.PP.KD", data=new_df)
-    plt.xlabel("Literacy Rate")
-    plt.ylabel("GDP PPP")
-    plt.title("Literacy rate and GDP PPP")
-    plt.show()
-
-# COL_1, COL_2, COL_3 = "UIS.LPP.AG15T99", "SE.ADT.LITR.MA.ZS", "Country Name" ###
-    new_df1 = df[["UIS.LPP.AG15T99", "SE.ADT.LITR.MA.ZS"]]
-
-    sns.scatterplot(x="UIS.LPP.AG15T99", y="SE.ADT.LITR.MA.ZS", data=new_df1)
-    plt.xlabel("Literacy Rate")
-    plt.ylabel("GDP PPP")
-    plt.title("male literacy and female literacy")
-    plt.show()
-
-
+    #
+    # new_df = df[["SE.ADT.LITR.ZS", "NY.GDP.PCAP.PP.KD", "Country Name"]]
+    # print(new_df["SE.ADT.LITR.ZS"].corr(new_df["NY.GDP.PCAP.PP.KD"]))
+    # sns.scatterplot(x="SE.ADT.LITR.ZS", y="NY.GDP.PCAP.PP.KD", data=new_df)
+    # plt.xlabel("Literacy Rate")
+    # plt.ylabel("GDP PPP")
+    # plt.title("Literacy rate and GDP PPP")
+    # plt.show()
 
     # COL_1, COL_2, COL_3 = "SE.PRM.ENRR", "SE.PRM.CMPT.ZS", "Country Name" ## "Gross enrolment ratio, primary, both sexes (%)" and "Primary completion rate, both sexes (%)"
-    # COL_1, COL_2, COL_3 = "SE.PRM.TCHR", "SE.PRM.ENRL.TC.ZS", "Country Name" ##"Teachers in primary education, both sexes (number)" and "Pupil-teacher ratio in primary education (headcount basis)"
-    # COL_1, COL_2, COL_3 = "SE.SEC.ENRL.VO.ZS", "UIS.GTVP.23.GPV", "Country Name" ##"Percentage of students in secondary education enrolled in vocational programmes, both sexes (%)" and "Percentage of students in secondary education enrolled in general programmes, both sexes (%)"
-    # COL_1, COL_2, COL_3 = "SE.PRM.REPT.ZS", "SE.PRM.PRSL.ZS", "Country Name" ##"Percentage of repeaters in primary education, all grades, both sexes (%)" and "Survival rate to the last grade of primary education, both sexes (%)"
-    # COL_1, COL_2, COL_3 = "SE.PRM.ENRR.FE", "SE.PRM.ENRR.MA", "Country Name" ##"Gross enrolment ratio, primary, female (%)" and "Gross enrolment ratio, primary, male (%)"
-
-
-    # COL_1, COL_2, COL_3 = "UIS.NERT.1", "SH.DYN.MORT", "Country Name" ##"Total net enrolment rate, primary, both sexes (%)" and "Mortality rate, under-5 (per 1,000)"
-    # COL_1, COL_2, COL_3 = "SE.PRM.ENRR", "SE.SEC.ENRR", "Country Name" ## Scatter Plot "Gross enrolment ratio, primary, both sexes (%)" vs "Gross enrolment ratio, secondary, both sexes (%)"
-    # COL_1, COL_2, COL_3 = "UIS.E.1.PR", "SE.SEC.ENRR", "Country Name"
-    # new_df = df[[COL_1, COL_2, COL_3]]
-    # new_df = new_df[new_df[COL_2] < 500]
-    # new_df = new_df[new_df[COL_1] > 80]
-
-    ######## BARCHART ########
-    # private, public, country = "UIS.E.1.PR", "UIS.E.1.PU", "Country Name"  ##Bar Chart "Enrolment in primary education, private institutions, both sexes (number)" vs "Enrolment in primary education, public institutions, both sexes (number)" by location
-    # new_df = df[[private, public, country]]
+    # new_df = df[["SE.PRM.ENRR", "SE.PRM.CMPT.ZS", "Country Name"]]
+    # sns.scatterplot(x="SE.PRM.ENRR", y="SE.PRM.CMPT.ZS", data=new_df)
     #
-    # fig = plt.figure(figsize=(10, 5))
+    # # for i, country in enumerate(new_df["Country Name"]):
+    # #     if country in country_list:
+    # #         plt.text(new_df["SE.PRM.ENRR"][i] + 0.2, new_df["SE.PRM.CMPT.ZS"][i] + 0.2, country)
     #
-    # plt.bar(new_df[country], new_df[private],
-    #         width=0.4)
-    # plt.bar(df[country], df[public],
-    #         width=0.4, bottom=public)
-    # plt.xlabel("Countries")
-    # plt.ylabel("No. of students enrolled")
-    # plt.title("Students enrolled in different courses")
+    # plt.xlabel("SE.PRM.ENRR")
+    # plt.ylabel("SE.PRM.CMPT.ZS")
+    # plt.title("Gross enrolment ratio, primary, both sexes (%) and Primary completion rate, both sexes (%)")
     # plt.show()
 
 
-    # high_df = df[df["Country Name"].isin(get_countries_with_income(["High income: nonOECD", "High income: OECD"]))]
-    # COL_1, COL_2, COL_3 = "SE.XPD.TOTL.GB.ZS", "NY.GDP.MKTP.CD", "Year"
-    # new_df = high_df[[COL_1, COL_2, COL_3, "Country Name"]]
-    # new_df.dropna(inplace=True)
-    # new_df["Spent on Edu"] = new_df[COL_1] * new_df[COL_2] / 100
+
+    # COL_1, COL_2, COL_3 = "SE.PRM.TCHR", "SE.PRM.ENRL.TC.ZS", "Country Name" ##"Teachers in primary education, both sexes (number)" and "Pupil-teacher ratio in primary education (headcount basis)"
+    # new_df = df[[COL_1, COL_2, COL_3]]
+    # sns.scatterplot(x=COL_1, y=COL_2, data=new_df)
+    # plt.xlabel("Literacy Rate")
+    # plt.ylabel("GDP PPP")
+    # plt.title("Literacy rate and GDP PPP")
+    # plt.show()
+
+    # COL_1, COL_2, COL_3 = "SE.SEC.ENRL.VO.ZS", "Year", "Country Name"                      1111111111111111111
+    # india_yearly = df.loc[df["Country Name"] == "India",  [COL_2, COL_1]].dropna()
+    # print(india_yearly)
+    # sns.barplot(x=COL_2, y=COL_1, data=india_yearly)
+    # plt.xlabel("%")
+    # plt.ylabel("Year")
+    # plt.title("Year wise stats of vocational enrollment")
+    # plt.show()
+
+# # 222222222222222222222222222222222222222222222222222
+#     COL_1, COL_2, COL_3 = "SE.PRM.REPT.ZS", "SE.PRM.PRSL.ZS", "Country Name" ##"Percentage of repeaters in primary education, all grades, both sexes (%)" and "Survival rate to the last grade of primary education, both sexes (%)"
+#     new_df = df[[COL_1, COL_2, COL_3]]
+#     sns.scatterplot(x=COL_1, y=COL_2, data=new_df)
+#     print(new_df["SE.PRM.REPT.ZS"].corr(new_df["SE.PRM.PRSL.ZS"]))
+#     plt.xlabel("Percentage of repeaters in primary education")
+#     plt.ylabel("Survival rate to the last grade of primary education")
+#     plt.title("percentage of repeaters and survival rate to last grade of primary education")
+#     plt.show()
+
+    # 33333333333333333333333333333333333333333333333333
+    # COL_1, COL_2, COL_3 = "SE.PRM.ENRR.FE", "Year", "Country Name" ##"Gross enrolment ratio, primary, female (%)"
+    # india_yearly = df.loc[df["Country Name"] == "India",  [COL_2, COL_1]].dropna()
+    # print(india_yearly)
+    # sns.barplot(x=COL_2, y=COL_1, data=india_yearly)
+    # plt.xlabel("Year")
+    # plt.ylabel("%")
+    # plt.title("Number of female enrollment in primary education in India year wise")
+    # plt.show()
+
+    #4444444444444444444444444444444444444444444444444
+    # COL_1, COL_2, COL_3 = "UIS.NERT.1", "SH.DYN.MORT", "Country Name" ##"Total net enrolment rate, primary, both sexes (%)" and "Mortality rate, under-5 (per 1,000)"
+    # new_df = df[[COL_1, COL_2, COL_3]]
+    # sns.scatterplot(x=COL_1, y=COL_2, data=new_df)
+    # print(new_df["UIS.NERT.1"].corr(new_df["SH.DYN.MORT"]))
+    # plt.xlabel("Total net enrolment rate, primary, both sexes (%")
+    # plt.ylabel("Mortality rate, under-5 (per 1,000)")
+    # plt.title("Total net enrolment rate, primary, both sexes (%) and Mortality rate, under-5 (per 1,000)")
+    # plt.show()
+    #5555555555555555555555555555555555555555555555555
+    # COL_1, COL_2, COL_3 = "SE.SEC.ENRR", "Year", "Country Name"
+    # new_df = df[[COL_1, COL_2, COL_3]]
+    # india_yearly = df.loc[df["Country Name"] == "India",  [COL_2, COL_1]].dropna()
+    # print(india_yearly)
+    # sns.barplot(x=COL_2, y=COL_1, data=india_yearly)
+    # plt.xlabel("Year")
+    # plt.ylabel("%")
+    # plt.title("Year wise Gross enrolment ratio, primary, both sexes (%) of India")
+    # plt.show()
+
+    #6666666666666666666666666666666666666666666666666
+    # COL_1, COL_2, COL_3 = "UIS.E.1.PR", "UIS.E.1.PU", "Year"
+    # new_df = df[[COL_1, COL_2, COL_3]]
+    # new_df = new_df.melt(value_vars=[COL_1, COL_2], id_vars=[COL_3]).dropna()
+    # print(new_df)
+    # sns.barplot(x="Year", y="value", data=new_df, hue="variable")
+    # plt.xlabel("Year")
+    # plt.ylabel("%")
+    # plt.title("Private and public enrollment in India")
+    # plt.show()
+
+
+    ######## BARCHART ########
+    #7777777777777777777777777777777777777777777777777
+    # enrollment_df = df[df["Country Name"].isin({"India", "Norway", "Brazil", "Japan"})]
+    # #
+    # COL_1, COL_2 = "SE.XPD.TOTL.GB.ZS", "Year"
+    # enrollment_df = enrollment_df[[COL_1, COL_2, "Country Name"]]
+    # enrollment_df.dropna(inplace=True)
     #
-    # high_grouped_df = new_df[["Year", COL_2, "Spent on Edu"]].groupby("Year").aggregate("sum")
-    # high_grouped_df["% Spent On Edu"] = high_grouped_df["Spent on Edu"] / high_grouped_df[COL_2] * 100
+    # enrollment_df = enrollment_df.pivot(index="Year", columns="Country Name", values=COL_1)[["India", "Norway", "Japan", "Brazil"]].dropna()
+    # enrollment_df = enrollment_df.melt(value_vars=["India", "Norway", "Japan", "Brazil"], ignore_index=False)
     #
-    # ## upper middle ##
-    #
-    # upper_middle_df = df[df["Country Name"].isin(get_countries_with_income(["Upper middle income"]))]
-    # new_um_df = upper_middle_df[[COL_1, COL_2, COL_3, "Country Name"]]
-    # new_um_df.dropna(inplace=True)
-    # new_um_df["Spent on Edu"] = new_um_df[COL_1] * new_um_df[COL_2] / 100
-    #
-    # um_grouped_df = new_um_df[["Year", COL_2, "Spent on Edu"]].groupby("Year").aggregate("sum")
-    # um_grouped_df["% Spent On Edu"] = um_grouped_df["Spent on Edu"] / um_grouped_df[COL_2] * 100
-    #
-    # ## lower middle ##
-    # lower_middle_df = df[df["Country Name"].isin(get_countries_with_income(["Lower middle income"]))]
-    # new_lm_df = lower_middle_df[[COL_1, COL_2, COL_3, "Country Name"]]
-    # new_lm_df.dropna(inplace=True)
-    # new_lm_df["Spent on Edu"] = new_lm_df[COL_1] * new_lm_df[COL_2] / 100
-    #
-    # lm_grouped_df = new_lm_df[["Year", COL_2, "Spent on Edu"]].groupby("Year").aggregate("sum")
-    # lm_grouped_df["% Spent On Edu"] = lm_grouped_df["Spent on Edu"] / lm_grouped_df[COL_2] * 100
-    #
-    # # low income ##
-    # low_df = df[df["Country Name"].isin(get_countries_with_income(["Low income"]))]
-    # new_l_df = low_df[[COL_1, COL_2, COL_3, "Country Name"]]
-    # new_l_df.dropna(inplace=True)
-    # new_l_df["Spent on Edu"] = new_l_df[COL_1] * new_l_df[COL_2] / 100
-    #
-    # l_grouped_df = new_l_df[["Year", COL_2, "Spent on Edu"]].groupby("Year").aggregate("sum")
-    # l_grouped_df["% Spent On Edu"] = l_grouped_df["Spent on Edu"] / l_grouped_df[COL_2] * 100
-    #
-    # combined_df = pd.concat([high_grouped_df["% Spent On Edu"], um_grouped_df["% Spent On Edu"], lm_grouped_df["% Spent On Edu"], l_grouped_df["% Spent On Edu"]], axis=1)
-    # combined_df.columns = ['High', 'Upper Middle', 'Lower Middle', 'Low']
-    #
-    # combined_df_melted = combined_df.melt(var_name='Groups', value_name='Values')
     # sns.set(style="whitegrid")
-    # sns.barplot(x='Groups', y='Values', data=combined_df_melted, hue='Groups')
-    # plt.title('Grouped Bar Plot of Columns')
-    # plt.xlabel('Income Group')
+    # sns.barplot(x=enrollment_df.index, y='value', data=enrollment_df, hue='Country Name')
+    # plt.title('')
+    # plt.xlabel('Year')
     # plt.ylabel('% Spent on Edu')
-    # plt.legend(title='Columns')
+    # plt.legend(title='Countries')
     # plt.show()
 
     ######## HISTOGRAM ########
-
+    #888888888888888888888888888888888888888888888888
     # plt.hist(df["SE.PRM.ENRL.TC.ZS"])
     # plt.xlabel("Pupil-teacher ratio")
     # plt.ylabel("Number of occurrence")
@@ -124,8 +151,14 @@ if __name__=="__main__":
     # plt.show()
 
     ######## BOX PLOT ########
-    # new_df = df[["SE.PRM.ENRR", "SE.SEC.ENRR", "SE.TER.ENRR"]]
-    # data = [new_df["SE.PRM.ENRR"].dropna().values, new_df["SE.SEC.ENRR"].dropna().values, new_df["SE.TER.ENRR"].dropna().values]
+    #999999999999999999999999999999999999999999999999
+    # COL_1, COL_2, COL_3 = "SE.PRM.ENRL.FE.ZS", "SE.SEC.ENRL.FE.ZS", "SE.TER.ENRL.FE.ZS"
+    # new_df = df[[COL_1, COL_2, COL_3, "Year", "Country Name"]]
+    # new_df = new_df[new_df["Year"] >= 2014]
+    #
+    # new_df.dropna(inplace=True)
+    #
+    # data = [new_df[COL_1].values, new_df[COL_2].values, new_df[COL_3].values]
     # plt.figure(figsize=(10, 6))
     # plt.boxplot(data, labels=['Primary', 'Secondary', 'Tertiary'])
     # plt.xlabel('Education Levels')
@@ -134,3 +167,91 @@ if __name__=="__main__":
     # plt.grid(True)
     # plt.tight_layout()
     # plt.show()
+
+    ## 10101010101100110101001010110010101010101010101010
+    # COL_1, COL_2, COL_3 = "IT.NET.USER.P2", "NY.GDP.PCAP.CD", "Country Name" ##Internet users per 100 people and GDP per capita
+    # new_df = df[[COL_1, COL_2, COL_3]]
+    # sns.scatterplot(x=COL_1, y=COL_2, data=new_df)
+    # print(new_df[COL_1].corr(new_df[COL_2]))
+    # # plt.xlabel("Total net enrolment rate, primary, both sexes (%")
+    # # plt.ylabel("Mortality rate, under-5 (per 1,000)")
+    # # plt.title("Total net enrolment rate, primary, both sexes (%) and Mortality rate, under-5 (per 1,000)")
+    # plt.show()
+
+    ## 11111111111111111111111111111111111111111111111111
+
+    # COL_1, COL_2, COL_3 = "NY.GDP.PCAP.CD", "SE.TER.ENRR", "Country Name" ##GDP per capita and Gross Enrolment Ratio in Tertiary Education
+    # new_df = df[[COL_1, COL_2, COL_3]]
+    # sns.scatterplot(x=COL_1, y=COL_2, data=new_df)
+    # print(new_df[COL_1].corr(new_df[COL_2]))
+    # # plt.xlabel("Total net enrolment rate, primary, both sexes (%")
+    # # plt.ylabel("Mortality rate, under-5 (per 1,000)")
+    # # plt.title("Total net enrolment rate, primary, both sexes (%) and Mortality rate, under-5 (per 1,000)")
+    # plt.show()
+
+
+    ##1212121212121212121212121212121212121212121212121212
+    # COL_1, COL_2, COL_3 = "SE.SEC.TCHR", "SE.SEC.ENRL" ##Enrolment in secondary education, both sexes, and teachers in secondary education, both sexes
+    # new_df = df[[COL_1, COL_2, COL_3]]
+    # sns.scatterplot(x=COL_1, y=COL_2, data=new_df)
+    # print(new_df[COL_1].corr(new_df[COL_2]))
+    # # plt.xlabel("Total net enrolment rate, primary, both sexes (%")
+    # # plt.ylabel("Mortality rate, under-5 (per 1,000)")
+    # # plt.title("Total net enrolment rate, primary, both sexes (%) and Mortality rate, under-5 (per 1,000)")
+    # plt.show()
+
+
+    ##1313131313131313131313131313131313131313131313131313
+    # COL_1, COL_2, COL_3 = "SE.PRE.ENRL","SP.PRE.TOTL.IN", "Country Name" ##Enrolment in pre-primary education and Population of the official age for pre-primary education, both sexes
+    # new_df = df[[COL_1, COL_2, COL_3]]
+    # sns.scatterplot(x=COL_1, y=COL_2, data=new_df)
+    # print(new_df[COL_1].corr(new_df[COL_2]))
+    # # plt.xlabel("Total net enrolment rate, primary, both sexes (%")
+    # # plt.ylabel("Mortality rate, under-5 (per 1,000)")
+    # # plt.title("Total net enrolment rate, primary, both sexes (%) and Mortality rate, under-5 (per 1,000)")
+    # plt.show()
+
+    ##1414141414141414141414141414141414141414141414141414
+    # COL_1, COL_2, COL_3 = "SE.PRE.ENRL","SP.PRE.TOTL.IN", "Country Name" ##Enrolment in pre-primary education and Population of the official age for pre-primary education, both sexes
+    # new_df = df[[COL_1, COL_2, COL_3]]
+    # sns.scatterplot(x=COL_1, y=COL_2, data=new_df)
+    # print(new_df[COL_1].corr(new_df[COL_2]))
+    # # plt.xlabel("Total net enrolment rate, primary, both sexes (%")
+    # # plt.ylabel("Mortality rate, under-5 (per 1,000)")
+    # # plt.title("Total net enrolment rate, primary, both sexes (%) and Mortality rate, under-5 (per 1,000)")
+    # plt.show()
+
+    ##15151515151515155155151515151515151515155511515151515
+    # COL_1, COL_2, COL_3 = "SE.SEC.ENRR.UP","NY.GDP.PCAP.CD", "Country Name" ## "Gross Enrolment Ratio, Upper Secondary, both sexes, and GDP per capita, current US$"
+    # new_df = df[[COL_1, COL_2, COL_3]]
+    # sns.scatterplot(x=COL_1, y=COL_2, data=new_df)
+    # print(new_df[COL_1].corr(new_df[COL_2]))
+    # # plt.xlabel("Total net enrolment rate, primary, both sexes (%")
+    # # plt.ylabel("Mortality rate, under-5 (per 1,000)")
+    # # plt.title("Total net enrolment rate, primary, both sexes (%) and Mortality rate, under-5 (per 1,000)")
+    # plt.show()
+
+    ##1616161616161616161616161616161611616161661616161611616
+
+    # COL_1, COL_2, COL_3 = "SP.TER.TOTL.IN","SE.TER.TCHR", "Country Name" ## "Population of the official age for tertiary education, both sexes, and Teachers in tertiary education, both sexes"
+    # new_df = df[[COL_1, COL_2, COL_3]]
+    # sns.scatterplot(x=COL_1, y=COL_2, data=new_df)
+    # print(new_df[COL_1].corr(new_df[COL_2]))
+    # # plt.xlabel("Total net enrolment rate, primary, both sexes (%")
+    # # plt.ylabel("Mortality rate, under-5 (per 1,000)")
+    # # plt.title("Total net enrolment rate, primary, both sexes (%) and Mortality rate, under-5 (per 1,000)")
+    # plt.show()
+
+    ## 171717171717171717171717171717117171717711717711717171
+
+    # COL_1, COL_2, COL_3 = "SE.PRM.TENR","SE.PRM.REPT.ZS", "Country Name" ## Adjusted net enrolment rate, primary, both sexes, and Percentage of repeaters in primary education, all grades, both sexes
+    # new_df = df[[COL_1, COL_2, COL_3]]
+    # sns.scatterplot(x=COL_1, y=COL_2, data=new_df)
+    # print(new_df[COL_1].corr(new_df[COL_2]))
+    # # plt.xlabel("Total net enrolment rate, primary, both sexes (%")
+    # # plt.ylabel("Mortality rate, under-5 (per 1,000)")
+    # # plt.title("Total net enrolment rate, primary, both sexes (%) and Mortality rate, under-5 (per 1,000)")
+    # plt.show()
+
+    ## 181818181818181818818181818181818181818181881818181818
+
